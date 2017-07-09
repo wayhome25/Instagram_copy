87b6c3ecd7dabb6773da9dfd7092b0716c257962
wayhome25 <siwabada@gmail.com>
2017-07-09 14:26:46 +0900

    post 앱 추가

---
+++ django_app/post/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Post(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class PostLike(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
