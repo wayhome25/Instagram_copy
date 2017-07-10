736cc098bce973fdcf6c817a590fec69c88c90fd
wayhome25 <siwabada@gmail.com>
2017-07-10 18:06:34 +0900

    signup view 로직 구현

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,9 +1,12 @@
-from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+from django.contrib.auth import \
+    authenticate, \
+    login as django_login, \
+    logout as django_logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 
-app_name = 'member'
+User = get_user_model()
 
 def login(request):
     """
@@ -33,3 +36,19 @@ def logout(request):
     """
     django_logout(request)
     return redirect('post:post_list')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if User.objects.filter(username=username).exists():
+            return HttpResponse('이미 존재하는 username 입니다')
+        elif password1 != password2:
+            return HttpResponse('비밀번호가 일치하지 않습니다')
+        user = User.objects.create_user(username=username, password=password1)
+        django_login(request, user)
+        return redirect('post:post_list')
+    else:
+        return render(request, 'member/signup.html')
