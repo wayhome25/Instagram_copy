4f9f0163c365d8f3c1bcc6682f7b1160fe5a75c9
wayhome25 <siwabada@gmail.com>
2017-07-11 13:16:40 +0900

    context_processors 추가

--- django_app/member/forms/login.py
+++ django_app/member/forms/login.py
@@ -5,9 +5,9 @@ from django.contrib.auth import authenticate
 class LoginForm(forms.Form):
 
     # todo label 뒤의 ':'를 삭제한다
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('label_suffix', '')
-        super().__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     kwargs.setdefault('label_suffix', '')
+    #     super().__init__(*args, **kwargs)
 
     username = forms.CharField(
         max_length=30,
